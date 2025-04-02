e18d54df0babd5020b50e54968aebd2455ed2943
corbin <corbinchristopher1@gmail.com>
2025-04-01 18:36:01 -0700

    init commit

---
+++ chunk.h
@@ -0,0 +1,16 @@
+#ifndef phx_chunk_h
+#define phx_chunk_h
+
+#include "common.h"
+
+typedef enum {
+    OP_RETURN,
+} OpCode;
+
+typedef struct {
+    int count;
+    int capacity;
+    uint8_t* code;
+} Chunk;
+
+#endif
---
+++ main.c
@@ -0,0 +1,5 @@
+#include "common.h"
+
+int main (int argc, cont char* argv[]) {
+    return 0;
+}
